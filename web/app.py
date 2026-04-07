cd29c93082ecc31f10d8a07c39c38a5373a06c5e
Kyle Thomsen <kylet@lifecconline.com>
2026-04-06 17:27:31 -0700

    fix: bind to 0.0.0.0: for Railway, add gunicorn

--- web/app.py
+++ web/app.py
@@ -279,4 +279,5 @@ def submitted():
 
 # ── Entry point ────────────────────────────────────────────────────────────────
 if __name__ == "__main__":
-    app.run(debug=True, port=5050)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", debug=False, port=port)
